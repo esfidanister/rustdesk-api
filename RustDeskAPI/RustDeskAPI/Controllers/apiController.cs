87292938db330432934b6306284ebebbdec4066b
liu1130768 <liu1130768@qq.com>
2023-10-04 23:31:59 +0800

    A

--- RustDeskAPI/RustDeskAPI/Controllers/apiController.cs
+++ RustDeskAPI/RustDeskAPI/Controllers/apiController.cs
@@ -80,41 +80,84 @@ namespace RustDeskAPI.Controllers
         {
             var Token = Request.Headers["authorization"].Replace("Bearer ", "").Trim();
             string sql = "";
-            abRequest abr = JsonConvert.DeserializeObject<abRequest>(data);
-            if (abr == null)
+            if (string.IsNullOrEmpty(value))
             {
-                return "{\"code\":99,\"data\":\"数据错误！\"}";
-            }
-            sql = string.Format("SELECT username,uid FROM `rustdesk_token` where access_token='{0}'", Token);
-            DataTable dt = MySQLHelper.QueryDataTable(sql);
-            List<rustdesk_token> tklst = JsonConvert.DeserializeObject<List<rustdesk_token>>(JsonConvert.SerializeObject(dt));
-            if (tklst.Count == 0)
-            {
-                return "{\"error\":\"Wrong credentials\",\"msg\":\"提供的登录信息错误\"}";
-            }
-            var uid = tklst.First().uid;
+                abRequest abr = JsonConvert.DeserializeObject<abRequest>(data);
+                if (abr == null)
+                {
+                    return "{\"code\":99,\"data\":\"数据错误！\"}";
+                }
+                sql = string.Format("SELECT username,uid FROM `rustdesk_token` where access_token='{0}'", Token);
+                DataTable dt = MySQLHelper.QueryDataTable(sql);
+                List<rustdesk_token> tklst = JsonConvert.DeserializeObject<List<rustdesk_token>>(JsonConvert.SerializeObject(dt));
+                if (tklst.Count == 0)
+                {
+                    return "{\"error\":\"Wrong credentials\",\"msg\":\"提供的登录信息错误\"}";
+                }
+                var uid = tklst.First().uid;
 
-            // 循环添加tags
-            sql = string.Format("DELETE FROM `rustdesk_tags` WHERE `uid` = '{0}';", uid);
-            MySQLHelper.Execute(sql);
-            foreach (var tag in abr.tags)
-            {
-                sql = string.Format("INSERT INTO `rustdesk_tags` (`uid`, `tag`) VALUES ('{0}', '{1}')", uid, tag);
+                // 循环添加tags
+                sql = string.Format("DELETE FROM `rustdesk_tags` WHERE `uid` = '{0}';", uid);
                 MySQLHelper.Execute(sql);
-            }
+                foreach (var tag in abr.tags)
+                {
+                    sql = string.Format("INSERT INTO `rustdesk_tags` (`uid`, `tag`) VALUES ('{0}', '{1}')", uid, tag);
+                    MySQLHelper.Execute(sql);
+                }
 
-            // 循环添加tags
-            sql = string.Format("DELETE FROM `rustdesk_peers` WHERE `uid` = '{0}';", uid);
-            MySQLHelper.Execute(sql);
-            foreach (var peer in abr.peers)
-            {
-                //rustdesk_peers peer = JsonConvert.DeserializeObject<rustdesk_peers>(v);
-                sql = string.Format("INSERT INTO `rustdesk_v2`.`rustdesk_peers` (`uid`, `client_id`, `username`, `hostname`, `alias`, `platform`, `tags`,`forceAlwaysRelay`,`rdpPort`,`rdpUsername`) "
-                    + "VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}')", uid, peer.id, peer.username, peer.hostname, peer.alias, peer.platform,
-                    JsonConvert.SerializeObject(peer.tags), peer.forceAlwaysRelay, peer.rdpPort, peer.rdpUsername);
+                // 循环添加tags
+                sql = string.Format("DELETE FROM `rustdesk_peers` WHERE `uid` = '{0}';", uid);
                 MySQLHelper.Execute(sql);
+                foreach (var peer in abr.peers)
+                {
+                    //rustdesk_peers peer = JsonConvert.DeserializeObject<rustdesk_peers>(v);
+                    sql = string.Format("INSERT INTO `rustdesk_v2`.`rustdesk_peers` (`uid`, `client_id`, `username`, `hostname`, `alias`, `platform`, `tags`,`forceAlwaysRelay`,`rdpPort`,`rdpUsername`) "
+                        + "VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}')", uid, peer.id, peer.username, peer.hostname, peer.alias, peer.platform,
+                        JsonConvert.SerializeObject(peer.tags), peer.forceAlwaysRelay, peer.rdpPort, peer.rdpUsername);
+                    MySQLHelper.Execute(sql);
+                }
+                return "{\"code\":100,\"data\": \"成功\"}";
             }
-            return "{\"code\":100,\"data\": \"成功\"}";
+            else if (value == "get")
+            {
+                sql = string.Format("SELECT username,uid FROM `rustdesk_token` where access_token='{0}'", Token);
+                DataTable dt = MySQLHelper.QueryDataTable(sql);
+                List<rustdesk_token> tklst = JsonConvert.DeserializeObject<List<rustdesk_token>>(JsonConvert.SerializeObject(dt));
+                if (tklst.Count == 0)
+                {
+                    return "{\"error\":\"Wrong credentials\",\"msg\":\"提供的登录信息错误\"}";
+                }
+                var tk = tklst.First();
+                sql = string.Format("SELECT tag FROM `rustdesk_tags` where uid = '{0}'", tk.uid);
+                dt = MySQLHelper.QueryDataTable(sql);
+                var tags = dt.AsEnumerable().Select(o => o.Field<string>("tag")).ToList();
+
+                sql = string.Format("SELECT * FROM `rustdesk_peers` where uid='{0}'", tk.uid);
+                dt = MySQLHelper.QueryDataTable(sql);
+                var peers = dt.AsEnumerable().Select(o => new
+                {
+                    id = o.Field<int>("id"),
+                    uid = o.Field<int>("uid"),
+                    client_id = o.Field<string>("client_id"),
+                    username = o.Field<string>("username"),
+                    hostname = o.Field<string>("hostname"),
+                    alias = o.Field<string>("alias"),
+                    platform = o.Field<string>("platform"),
+                    tags = JsonConvert.DeserializeObject<List<string>>(o.Field<string>("tags")),
+                    forceAlwaysRelay = o.Field<string>("forceAlwaysRelay"),
+                    rdpPort = o.Field<string>("rdpPort"),
+                    rdpUsername = o.Field<string>("rdpUsername"),
+                }).ToList();
+                var result = new
+                {
+                    data = new
+                    {
+                        tags,
+                        peers
+                    }
+                };
+            }
+            return "";
         }
 
         [HttpGet]
